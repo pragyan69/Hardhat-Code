0fc89fffbfb27a977430ee4741e986d562ae65f8
pragyan borah <pragyanborah@pragyans-MacBook-Air.local>
2023-03-27 13:22:29 +0530

    added hardhat code

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+
+require("@nomicfoundation/hardhat-toolbox");
+
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "KEY";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY = "YOUR SEPOLIA PRIVATE KEY";
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
+};
+
+
+//npx hardhat run scripts/deploy.js --network sepolia  
